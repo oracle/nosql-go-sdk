0d856d7558fc6443ad7ccf818f80b72ba8ac84a8
John Connelly <john.connelly@oracle.com>
2020-03-06 10:45:50 -0800

    Added top-level doc file

---
+++ nosql.go
@@ -0,0 +1,29 @@
+//
+// Copyright (C) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
+//
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+//
+// Please see LICENSE.txt file included in the top-level directory of the
+// appropriate download for a copy of the license and additional information.
+//
+
+/*
+This is the official Go SDK for Oracle NoSQL.
+
+More detailed information can be viewed at: https://github.com/oracle/nosql-go-sdk/blob/master/README.md
+
+Installation
+
+Refer to https://github.com/oracle/nosql-go-sdk/blob/master/README.md#installation for installation instructions.
+
+Configuration
+
+Refer to https://github.com/oracle/nosql-go-sdk/blob/master/README.md#configuring-the-sdk for configuration instructions.
+
+Full Example
+
+See https://github.com/oracle/nosql-go-sdk/blob/master/README.md#simple-example for an example program that uses the Go SDK to interact with an Oracle NoSQL database.
+
+
+*/
+package nosql
